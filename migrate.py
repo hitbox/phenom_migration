6931c5352ddf8bfbde117546be53ab0882d74fab
Carl L. Harris <Carl.Harris@airborneglobal.com>
2025-12-03 15:43:47 -0500

    development

--- migrate.py
+++ migrate.py
@@ -7,7 +7,32 @@ from pprint import pprint
 from requests.auth import HTTPBasicAuth
 from itertools import permutations
 
+from zeep import Client
+from zeep.transports import Transport
+
 logger = logging.getLogger('migrate')
+
+def get_signin(tenant, client_id, client_secret):
+    signin_url = f'https://signin.ultipro.com/signin/oauth2/t/{tenant}/access_token'
+
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "accept": "application/json",
+    }
+
+    payload = {
+        'grant_type': 'client_credentials',
+        'client_id': client_id,
+        'client_secret': client_secret,
+        # From email subject:
+        # "ATSG - Request for ats name Staging and Production Endpoints"
+        #'scope': scope,
+    }
+    response = requests.post(signin_url, data=payload, headers=headers)
+    response.raise_for_status()
+
+    data = response.json()
+    return data
 
 def main(argv=None):
     parser = argparse.ArgumentParser()
@@ -15,6 +40,7 @@ def main(argv=None):
     args = parser.parse_args(argv)
 
     logging.basicConfig(
+        filename = 'output.log',
         level = logging.DEBUG,
     )
 
@@ -30,35 +56,30 @@ def main(argv=None):
     hostname = appconfig['hostname']
     client_secret = appconfig['client_secret']
     tenant = appconfig['tenant']
-    scope = appconfig['scope']
+    userkey = appconfig['userkey']
+    password = appconfig['password']
+    userkey = appconfig['userkey']
+    username = appconfig['username']
 
-    signin_url = f'https://signin.ultipro.com/signin/oath2/t/{tenant}/access_token'
+    signin_data = get_signin(tenant, client_id, client_secret)
+    access_token = signin_data['access_token']
 
-    headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "accept": "application/json",
-    }
+    session = requests.Session()
+    session.headers.update({'Authorization': f'Bearer {access_token}'})
+    transport = Transport(session=session)
 
-    payload = {
-        'grant_type': 'client_credentials',
-        'client_id': client_id,
-        'client_secret': client_secret,
-        # From email subject:
-        # "ATSG - Request for ats name Staging and Production Endpoints"
-        'scope': scope,
-    }
-    response = requests.post(signin_url, data=payload, headers=headers)
-    response.raise_for_status()
-
-    print(f'{signin_url} {response.json()=}')
-
-    endpoint_url = (
-        f'https://{hostname}/talent/recruiting/v2'
-        f'/signin/oauth2/t/{tenant}/access_token'
+    client = Client(
+        wsdl='https://service2.ultipro.com/services/BIDataService?wsdl',
+        transport=transport,
     )
 
-    print('payload:')
-    pprint(payload)
+    result = client.service.LogOn(
+        logOnRequest = {
+            'UserName': username,
+            'Password': password,
+        },
+    )
+    logger.debug(result)
 
 if __name__ == "__main__":
     main()
